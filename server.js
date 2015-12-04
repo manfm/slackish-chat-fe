470e676df30c04f59f67e1a62be3e1327a5d0b92
Martin Chapcak <chapcak.m@gmail.com>
2015-12-04 20:47:09 +0900

    Heroku

---
+++ server.js
@@ -0,0 +1,14 @@
+// basic server for static files
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 8000);
+
+// pass ENV variables to js script
+app.get('/getenv.js', function(req, res){
+  if(process.env.REST_API_URL){
+    res.send("var REST_API_URL='"+process.env.REST_API_URL+"';");
+  } else {
+    res.send("");
+  }
+});
