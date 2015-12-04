0fd0c97d70a98266a1cd6ff80a1bff757cba19b9
Martin Chapcak <chapcak.m@gmail.com>
2015-12-05 08:26:21 +0900

    Init ng-token-auth

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,19 +1,33 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['ngRoute', 'ng-token-auth'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider
-    .when('/login', {
-      templateUrl: 'view1/login.html',
-      controller: 'LoginCtrl'
-    })
-    .when('/signup', {
-      templateUrl: 'view1/signup.html',
-      controller: 'LoginCtrl'
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'view1/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/signup', {
+        templateUrl: 'view1/signup.html',
+        controller: 'LoginCtrl'
+      });
+  }])
+  .config(function($authProvider) {
+    $authProvider.configure({
+      apiUrl: 'http://api.example.com'
     });
-}])
-
-.controller('LoginCtrl', ['$scope', '$location', function($scope, $location) {
-  $scope.user = [];
-}]);
+  })
+  .controller('LoginCtrl', ['$scope', '$location', '$auth',
+    function($scope, $location, $auth) {
+      $scope.handleLoginBtnClick = function() {
+        $auth.submitLogin($scope.loginForm)
+          .then(function(resp) {
+            // handle success response
+          })
+          .catch(function(resp) {
+            // handle error response
+          });
+      };
+    }
+  ]);
