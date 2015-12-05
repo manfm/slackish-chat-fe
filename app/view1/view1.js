da2da3131a57039628c1d09ceec278ad70de40c1
Martin Chapcak <chapcak.m@gmail.com>
2015-12-05 09:45:14 +0900

    Sign Up + Sign In

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -15,11 +15,21 @@ angular.module('myApp.view1', ['ngRoute', 'ng-token-auth'])
   }])
   .config(function($authProvider) {
     $authProvider.configure({
-      apiUrl: 'http://api.example.com'
+      apiUrl: (typeof REST_API_URL !== 'undefined') ? REST_API_URL : 'http://localhost:5000'
     });
   })
   .controller('LoginCtrl', ['$scope', '$location', '$auth',
     function($scope, $location, $auth) {
+      $scope.handleRegBtnClick = function() {
+        $auth.submitRegistration($scope.registrationForm)
+          .then(function(resp) {
+            // handle success response
+          })
+          .catch(function(resp) {
+            // handle error response
+          });
+      }
+      
       $scope.handleLoginBtnClick = function() {
         $auth.submitLogin($scope.loginForm)
           .then(function(resp) {
