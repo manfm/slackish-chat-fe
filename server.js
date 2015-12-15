ee26eec36ee32baae12dde40d0f74adf96487027
Martin Chapcak <chapcak.m@gmail.com>
2015-12-15 19:23:00 +0900

    Fixed getenv.js resource for loading ENV vars into js app

--- server.js
+++ server.js
@@ -7,12 +7,12 @@ app.listen(process.env.PORT || 8000);
 // pass ENV variables to js script
 app.get('/getenv.js', function(req, res) {
   if (process.env.REST_API_URL) {
-    res.send("var REST_API_URL='" + process.env.REST_API_URL + "';");
+    res.write("var REST_API_URL='" + process.env.REST_API_URL + "';");
   }
 
   if (process.env.WEBSOCKET_URL) {
-    res.send("var WEBSOCKET_URL='" + process.env.WEBSOCKET_URL + "';");
+    res.write("var WEBSOCKET_URL='" + process.env.WEBSOCKET_URL + "';");
   }
 
-  res.send("");
+  res.end();
 });
